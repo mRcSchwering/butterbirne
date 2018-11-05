5bcfec392ed2be81f90b044ffbafdb4bcf0d23a8
Marc Schwering <schweringmarc01@gmail.com>
2018-11-05 21:31:07 +0100

    param generator

--- experiments/dowJones_histFeatures_10y_fullYears/xgp_tryout.py
+++ experiments/dowJones_histFeatures_10y_fullYears/xgp_tryout.py
@@ -2,34 +2,7 @@
 import xgboost as xgb
 import pandas as pd
 
-df = pd.DataFrame({'a': ['a', 'b', 'c']})
-isins = ['x', 'y']
-tps = [2, 2, 3]
-w = 2
-
-
-isins = ['a', 'b', 'c']
-w = 'd1'
-['x', 'y'] * 3
-
-
-l = []
-'-'.join(df.iloc[:,0])
-
-l.append(df)
-l.append(df)
-pd.concat(l, ignore_index=True)
-
-
-
-from finData.abtCreator import WindowIterator
-
-# TODO Klasse(n) für X, Y Berechnung pro window, Feature Berechnung möglichst flexibel
-# TODO Klasse für Window shifting
-# TODO Klasse die alle windows zu einer großen X und Y zusammen baut
-# -> ABT als csv oder vllt als DMatrix direkt
-# erst mal für diesen use case
-# -> refaktorisieren, gucken ob es für fundamental wiederverwendbar wäre
+# TODO Klasse für hyperparameter tuning
 
 # TODO Klasse für Train/Val Trennung
 # TODO Klasse für Repeated CrossVal
@@ -37,134 +10,63 @@ from finData.abtCreator import WindowIterator
 # TODO richtige Vola Berechnung
 
 workingDir = './experiments/dowJones_histFeatures_10y_fullYears/'
-data = pd.read_csv(workingDir + 'postDQR_long.csv')
-isins = list(set(data['isin']))
+X = pd.read_csv(workingDir + 'X.csv', index_col=0)
+Y = pd.read_csv(workingDir + 'Y.csv', index_col=0)
 
-data['steps'] = data['year'].map(str) + '-' + data['month'].map(str)
-stepCol = 'steps'
-
-steps = []
-for year in sorted(set(data['year'])):
-    for month in sorted(set(data['month'])):
-        steps.append(str(year) + '-' + str(month))
-
-windows = WindowIterator(steps, 3)
-
-df = pd.DataFrame({'a': [1, 2, 3], 'b': [1, 2, 3]})
-df['c'] = df['a'] + df['b']
-df
-
-
-# Klasse braucht
-# isins
-# data
-ISINS = isins
-DATA = data
-Y_ = getTargetForWindow(w_y, calculateY)
-
-
-def calculateY(isins, df):
-    y = []
-    for isin in isins:
-        cond = (df['isin'] == isin) & (df['variable'] == 'logReturn')
-        y.append(sum(df.loc[cond]['value']))
-    quants = pd.np.quantile(y, [0.25, 0.75])
-    return [int(d > quants[1]) for d in y]
-
-abtc = ABTcreator(data, isins, windows, calculateY)
-
-Y = abtc.calculateABT()
-Y.keys()
-df = pd.DataFrame({'tp': [1, 2]})
-df['a'] = 'a'
-
-
-type(data)
-data.loc[data['month'] == [1, 2]]
-
-
-def a(a):
-    return a
-
-class ABTcreator(object):
-
-    def __init__(self, data, isins, windows, getY):
-        self._data = data
-        self._isins = isins
-        self._windows = windows
-        self._getY = getY
-
-    def calculateABT(self):
-        Y = {}
-        for w in self._windows:
-            w_y = w[len(w) - 1]
-            y = self._getTargetForWindow(w_y, self._getY)
-            Y[w_y] = y
-        return Y
-
-    def _getTargetForWindow(self, w, getY, step='steps', target='target'):
-        Y = pd.DataFrame(index=self._isins)
-        df = self._data.loc[self._data[step] == w]
-        Y[target] = getY(self._isins, df)
-        return Y
-
-
-[1,2,3][:-1]
-
-
-
-
-
-
-T = set([d for d in data['year']])  # all steps available
-W = [2014, 2015, 2016]  # window used: last position is predicted
-n = len(T)  # number of steps available
-m = len(W)  # window size: position in W = distance to prediction position
-n - m + 1  # possible time intervals
-
-# calculate features for window
-X = pd.DataFrame(index=isins)
-for step in range(m - 1):
-    p = m - 1 - step
-    df = data.loc[data['year'] == W[step]]
-
-    x1 = []
-    x2 = []
-    for isin in isins:
-        cond1 = (df['isin'] == isin) & (df['variable'] == 'logReturn')
-        cond2 = (data['isin'] == isin) & (data['variable'] == 'logVola')
-        x1.append(sum(df.loc[cond1]['value']))
-        x2.append(sum(data.loc[cond2]['value']))
-    X['sumLogReturn' + str(p)] = x1
-    X['sumLogVola' + str(p)] = x2
-
-
+Xtrain = X[:200]
+Ytrain = Y[:200]
+Xval = X[200:300]
+Yval = Y[200:300]
+Xtest = X[300:400]
+Ytest = Y[300:400]
 
 
 # idee ranking objectives ausprobieren
 # 2 logregs jeweils gegen gut und schlecht
-dtrain = xgb.DMatrix(X, label=Y)
+dtrain = xgb.DMatrix(Xtrain, label=Ytrain.isUpperQuart)
+dval = xgb.DMatrix(Xval, label=Yval.isUpperQuart)
 
-param = {'max_depth': 2, 'eta': 1, 'silent': 1, 'objective': 'binary:logistic'}
+param = {
+    'max_depth': 7,
+    'gamma': .3,
+    'eta': .01,
+    'silent': 1,
+    'objective': 'binary:logistic',
+    'subsample': 1,
+    'colsample_bytree': 1,
+    'alpha': 0,
+}
 param['nthread'] = 3
 param['eval_metric'] = 'auc'
 
 # gibt hier nochmal test an, reicht auch nur train?
-evallist = [(dtest, 'eval'), (dtrain, 'train')]
+evallist = [(dval, 'eval'), (dtrain, 'train')]
 
-num_round = 10
-bst = xgb.train(param, dtrain, num_round)
+num_round = 10000
+bst = xgb.train(param, dtrain, num_round, evallist, early_stopping_rounds=100)
+
+# cv mit eval:auc scheint nicht zu gehen
+# man kann aber eigene FUnktion für eval definieren:
+#https://github.com/dmlc/xgboost/issues/1226
+# das scheint dann zu gehen (gibt feval, maximize)
+bst = xgb.cv(param, dtrain, num_boost_round=num_round, nfold=5, metrics=['auc'])
 # mit evallist (4. arg) kann ich dann auch early_stopping_rounds=x eintragen
 # eval metric wird für early stopping verwendet
+# mit verbose_eval=False hält der die fresse
+# mal eval_metrics angucken
 
 
-dtest = xgb.DMatrix(X)
+
+dtest = xgb.DMatrix(Xtest)
 ypred = bst.predict(dtest)
+sum(ypred[list(Ytest.isUpperQuart == 1)] > 0.1) / sum(list(Ytest.isUpperQuart == 1))
 
-fig = xgb.plot_importance(bst).figure
-fig.set_size_inches(12, 8)
-fig
+bst.attributes()
+bst.attr('best_iteration')
+dir(bst)
+3*3*5*20 * 10 /60/60
 
-fig = xgb.plot_tree(bst, num_trees=2).figure
-fig.set_size_inches(12, 8)
-fig
+
+xgb.plot_importance(bst).figure.set_size_inches(12, 8)
+
+xgb.plot_tree(bst, num_trees=2).figure.set_size_inches(12, 8)
---
+++ finData/parameterGenerator.py
@@ -0,0 +1,86 @@
+# This Python file uses the following encoding: utf-8
+import pandas as pd
+import random
+
+
+class ParameterGenerator(object):
+    """
+    Do random search for parameters with primitive feature of taking difference
+    between train and test metrics into consideration.
+    Use next() and getResults()
+    """
+
+    parameters = {
+        'max_depth': {
+            'range': [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            'start': 6
+        },
+        'subsample': {
+            'range': [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8],
+            'start': 0.5
+        },
+        'colsample_bytree': {
+            'range': [0.5, 0.6, 0.7, 0.8, 0.9, 1],
+            'start': 0.8
+        }
+    }
+
+    def __init__(self):
+        self.i = 0
+        for param in self.parameters:
+            self.parameters[param]['value'] = self.parameters[param]['start']
+        self._createCaptureLists()
+
+    def next(self, train, test):
+        """
+        Provide train and test results from previous run
+        to get next set of parameters.
+        """
+        self._capture['iter'].append(self.i)
+        self._capture['train'].append(train)
+        self._capture['test'].append(test)
+        newValues = self._getNewValues(train, test)
+        self._updateParameters(newValues)
+        self.i += 1
+        return newValues
+
+    def getResults(self, train, test):
+        """
+        Provide train and test results from previous run
+        and get summary of parameter sets and results as DataFrame.
+        """
+        self._capture['iter'].append(self.i)
+        self._capture['train'].append(train)
+        self._capture['test'].append(test)
+        return pd.DataFrame(self._capture)
+
+    def _getNewValues(self, train, test):
+        vals = dict()
+        isTooComplex = self._modelTooComplex(train, test)
+        for param in self.parameters:
+            vals[param] = self._getNewValue(param, isTooComplex)
+        return vals
+
+    def _getNewValue(self, param, isTooComplex):
+        if param == 'max_depth' and isTooComplex:
+            preVal = self._capture[param][-1]
+            pop = [d for d in self.parameters[param]['range'] if d < preVal]
+            return random.sample(pop, 1)[0]
+        return random.sample(self.parameters[param]['range'], 1)[0]
+
+    def _updateParameters(self, values):
+        for param in self.parameters:
+            self._capture[param].append(self.parameters[param]['value'])
+            self.parameters[param]['value'] = values[param]
+
+    def _createCaptureLists(self):
+        cap = {'iter': [], 'train': [], 'test': []}
+        for param in self.parameters:
+            cap[param] = [self.parameters[param]['value']]
+        self._capture = cap
+
+    @classmethod
+    def _modelTooComplex(cls, metricTrain, metricTest):
+        if metricTrain <= 0:
+            return False
+        return True if metricTest/metricTrain > 1.1 else False
